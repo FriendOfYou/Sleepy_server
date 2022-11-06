84da58fdaf05a989334b52948bd3ff5061fe6ee4
枫凡 赵 <275947878@qq.com>
2022-11-07 00:00:59 +0800

    q

--- app/person_partners.py
+++ app/person_partners.py
@@ -13,7 +13,7 @@ def person_partner(person_id):
         persons_data = []  # 存放最新的电影数据
         for i in range(len(data)):  # 添加电影数据
             person_data = search_personDetail(data[i][0])
-            person = {'times': data[i][1], 'id': person_data[i][0], 'name': person_data[i][1], 'sex': person_data[i][3],
+            person = {'times': int(data[i][1]), 'id': person_data[i][0], 'name': person_data[i][1], 'sex': person_data[i][3],
                       'birthday': person_data[i][4], 'birthplace': person_data[i][5], 'summary': person_data[i][6],
                       'img': person_data[i][2]}
             persons_data.append(person)
