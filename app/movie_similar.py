b626f2d251877d5b7b0438294358553db47278f3
枫凡 赵 <275947878@qq.com>
2022-11-08 00:39:30 +0800

    q

--- app/movie_similar.py
+++ app/movie_similar.py
@@ -9,30 +9,26 @@ from app.mysql_data import search_movieSimilar, get_Genre, get_Country
 
 @app.route('/movie/<movie_id>/similar', methods=['POST', 'GET'])
 def movie_similar(movie_id):
-    movie = search_movieSimilar(movie_id)  # 三个（至多）相似的电影数据
-    if movie != 0:
-        movie_data = []  # 存放相似的电影数据
-        for i in range(len(movie)):  # 添加电影数据
+    data = search_movieSimilar(movie_id)
+    if data != 0:
+        movie_data = []
+        for i in range(len(data)):  # 添加电影数据
             # 将带电影所需体裁获取
-            genre = get_Genre(movie[i][0])
+            genre = get_Genre(data[i][0])
             genres = []
             for j in range(len(genre)):
                 genre_data = {'id': genre[j][1], 'name': genre[j][2]}  # 体裁的id和体裁名称
                 genres.append(genre_data)
             # 获取电影所属国家/地区信息
-            country = get_Country(movie[i][0])
+            country = get_Country(data[i][0])
             countries = []
             for j in range(len(country)):
                 country_data = {'id': country[j][1], 'name': country[j][2]}  # 所属国家/地区的id和名称
                 countries.append(country_data)
-            movie = {'id': movie[i][0], 'name': movie[i][1], 'year': movie[i][2], 'rating': movie[i][3],
-                     'img': movie[i][5], 'tags': movie[i][6], 'desc': movie[i][7], 'genre': genres,
-                     'country': countries}
+            movie = {'id': data[i][0], 'name': data[i][1], 'year': data[i][2], 'rating': data[i][3], 'img': data[i][5],
+                     'tags': data[i][6], 'desc': data[i][7], 'genre': genres, 'country': countries}
             movie_data.append(movie)
-
-        return Response(json.dumps({'status': 0, 'msg': "相似电影查询成功",
-                                    'data': movie_data}),
+        return Response(json.dumps({'status': 0, 'msg': "影人最新5部电影返回成功", 'data': movie_data}),
                         content_type='application/json')
     else:
-        return Response(json.dumps({'status': 1, 'msg': "相似查询失败"}),
-                        content_type='application/json')
+        return Response(json.dumps({'status': 1, 'msg': "影人最新5部电影返回失败"}), content_type='application/json')
