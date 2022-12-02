e587c1b50916622eed2c6e9b32910059a256ff13
枫凡 赵 <275947878@qq.com>
2022-12-02 21:22:17 +0800

    判断了生成的电影是否存在，同时对一些传回数据的问题进行了修改。

--- app/movie_suggest.py
+++ app/movie_suggest.py
@@ -42,22 +42,23 @@ def movie_suggest():
         movie_data = []
         for i in range(len(Recommendations)):  # 添加电影数据
             movie = search_movieDetail(Recommendations[i][0])
-            # 将带电影所需体裁获取
-            genre = get_Genre(Recommendations[i][0])
-            genres = []
-            for j in range(len(genre)):
-                genre_data = {'id': genre[j][1], 'name': genre[j][2]}  # 体裁的id和体裁名称
-                genres.append(genre_data)
-            # 获取电影所属国家/地区信息
-            country = get_Country(Recommendations[i][0])
-            countries = []
-            for j in range(len(country)):
-                country_data = {'id': country[j][1], 'name': country[j][2]}  # 所属国家/地区的id和名称
-                countries.append(country_data)
-            movie_one = {'id': movie[i][0], 'name': movie[i][1], 'year': movie[i][2], 'rating': movie[i][3],
-                         'img': movie[i][5],
-                         'tags': json5.loads(movie[6]), 'desc': movie[i][7], 'genre': genres, 'country': countries}
-            movie_data.append(movie_one)
+            if movie != 0:
+                # 将带电影所需体裁获取
+                genre = get_Genre(Recommendations[i][0])
+                genres = []
+                for j in range(len(genre)):
+                    genre_data = {'id': genre[j][1], 'name': genre[j][2]}  # 体裁的id和体裁名称
+                    genres.append(genre_data)
+                # 获取电影所属国家/地区信息
+                country = get_Country(Recommendations[i][0])
+                countries = []
+                for j in range(len(country)):
+                    country_data = {'id': country[j][1], 'name': country[j][2]}  # 所属国家/地区的id和名称
+                    countries.append(country_data)
+                movie_one = {'id': movie[0], 'name': movie[1], 'year': movie[2], 'rating': movie[3],
+                             'img': movie[5],
+                             'tags': json5.loads(movie[6]), 'desc': movie[7], 'genre': genres, 'country': countries}
+                movie_data.append(movie_one)
         return Response(json.dumps({'status': 0, 'msg': "返回成功", 'data': movie_data}),
                         content_type='application/json')
     else:
